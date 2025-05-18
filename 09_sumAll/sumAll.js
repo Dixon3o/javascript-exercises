a5f121e18cfacd493f517495c81a17df5b8b22ef
Dixon3o <dimitrije.bibic64@gmail.com>
2025-05-18 23:24:46 +0200

    done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)){
+        return 'ERROR';
+    }
+    if(start <0 || end < 0){
+        return 'ERROR';
+    }
+    if(typeof start !== 'number' || typeof end !=='number'){
+        return 'ERROR';
+    }
 
+    let finalsum = 0;
+    if (start > end){
+        for (let i = start; i >= end; i--){
+        finalsum += i;
+        }   
+    }else{
+        for (let i = start; i <= end; i++){
+            finalsum += i;
+        }
+    }
+    return finalsum;
 };
+console.log(sumAll(123, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
