71a2a1b576f6b208e87c1699f04adad65c482950
Dixon3o <dimitrije.bibic64@gmail.com>
2025-05-18 22:51:01 +0200

    Done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numsToRemove) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++){
+        if (!numsToRemove.includes(array[i])){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
+console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
