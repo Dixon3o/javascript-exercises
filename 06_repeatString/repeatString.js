20b7a9b443006cb174f6148f1cc1d4395f361f3a
Dixon3o <dimitrije.bibic64@gmail.com>
2025-05-18 20:22:38 +0200

    Done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = '';
+    if (num < 0){
+        newString = 'ERROR'
+        console.log(newString);
+    }else if(num >= 0){
+        for (let i = 0; i < num; i++){
+            newString += string;
+        }
+    }
+    
+    return newString;
 };
+console.log(repeatString('hey', 3));
+
 
 // Do not edit below this line
 module.exports = repeatString;
