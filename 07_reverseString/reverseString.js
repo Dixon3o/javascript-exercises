ae356218899f29b06257acc979b1d604d2395782
Dixon3o <dimitrije.bibic64@gmail.com>
2025-05-18 21:13:42 +0200

    Done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string){
+    let newString = '';
+    for (let i = string.length -1; i >= 0; i--){
+        let char = string.charAt(i);
+        newString += char;
+    }
+    return newString;
 };
+console.log(reverseString(''));
 
 // Do not edit below this line
 module.exports = reverseString;
