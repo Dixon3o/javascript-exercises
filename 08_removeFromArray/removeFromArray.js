af7f6f0602993604e341208e43b015eeaa8d032d
Dixon3o <dimitrije.bibic64@gmail.com>
2025-05-18 22:55:31 +0200

    Done with arrow function now

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,11 +1,5 @@
-const removeFromArray = function(array, ...numsToRemove) {
-    let newArray = [];
-    for (let i = 0; i < array.length; i++){
-        if (!numsToRemove.includes(array[i])){
-            newArray.push(array[i]);
-        }
-    }
-    return newArray;
+const removeFromArray = (array, ...numsToRemove) => {
+    return array.filter(element => !numsToRemove.includes(element));
 };
 console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4));
 
