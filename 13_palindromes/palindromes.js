3c52a741cf2296769012c3e412c3932c2342cb83
Dixon3o <dimitrije.bibic64@gmail.com>
2025-06-03 11:16:02 +0200

    Done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversed = cleanString.split('').reverse().join('');
+    if(cleanString === reversed){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
