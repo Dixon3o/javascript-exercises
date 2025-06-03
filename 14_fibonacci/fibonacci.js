f093a8c5d9951a8c23e20e2bcda3547282312ac6
Dixon3o <dimitrije.bibic64@gmail.com>
2025-06-03 18:33:34 +0200

    Done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    let first = 0;
+    let second = 1;
+    let current;
+    if(num === 0){
+        return current = 0;
+    }else if(num < 0){
+        let msg = 'OOPS';
+        return msg;
+    }else if(num === 1){
+        return current = 1;
+    }
+    for(let i = 2; i <= num; i++){
+        current = first + second;
+        first = second;
+        second = current;
+        
+    }
+    return current;
 };
 
 // Do not edit below this line
