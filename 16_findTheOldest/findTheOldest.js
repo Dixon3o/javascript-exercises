1a30b79c238e0487a8762b2a73597c3164fa6dc4
Dixon3o <dimitrije.bibic64@gmail.com>
2025-06-04 14:31:20 +0200

    Done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    function getAge(person){
+        if(person.yearOfDeath !== undefined){
+            return person.yearOfDeath - person.yearOfBirth;
+        }else{
+            let currentYear = new Date().getFullYear();
+            return currentYear - person.yearOfBirth;
+        }
+    }
+    return arr.reduce((a,b) => {
+        if(getAge(a) > getAge(b)){
+            return a;
+        }else{
+            return b;
+        }
+    });
 };
 
 // Do not edit below this line
